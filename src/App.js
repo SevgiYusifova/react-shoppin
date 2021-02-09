e5ced05f16ac7554d001dfa4f9864a1d972d2903
Sevgi <sevgiyusifova98@gmail.com>
2021-02-09 23:03:29 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="grid-container">
+
+      <header>
+        <a href="/">React shopping cart</a>
+      </header>
+
+      <main>
+        Product
+      </main>
+
+      <footer>
+        All rights is reserved.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
